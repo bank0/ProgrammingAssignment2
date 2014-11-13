31adcf8e0536733c032a60a9a1d4042cb144464f
bank0 <>
2014-11-13 12:28:00 +0100

    Add function definitions for caching the inverse of a matrix
    Implements makeCachMatrix and cacheSolve as helper functions to
    save time when calculating the inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+############################################################################
+## Functions developed part of R programming course held by Johns Hopkins ##
+## Bloomberg School of Public Health at coursera.org.                     ##
+## Helper functions implemented to potentially save time at the costly    ##
+## operation of inverting a matrix.                                       ##
+############################################################################
 
-## Write a short comment describing this function
-
+#' Holds a matrix together with its cached inverse.
+#'
+#' @param x A matrix initialized by matrix()
+#' @return A list of functions moderating a matrix and its inverted equivalent
+#' @examples
+#' m <- makeCacheMatrix(matrix(1:4,2,2))
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function() inv
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+#' Returns the inverse of a cached matrix.
+#' 
+#' @param x A cached matrix created with @makeCacheMatrix
+#' @return The inverse of the cached matrix(x)
+#' @examples
+#' 
+#' m<-matrix(trunc(rnorm(2048*2048)), 2048,2048)
+#' m_cached <- makeCacheMatrix(m)
+#' system.time(cacheSolve(m_cached))
+#'   user  system elapsed 
+#'   7.95    0.03    7.98
+#'
+#' system.time(cacheSolve(m_cached))
+#' getting cached data
+#'    user  system elapsed 
+#'       0       0       0
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinverse(inv)
+        inv
 }
